d8f923004243dfa88474b49777148e3a94bd5aef
AliAsgharSWE <stylinalivlogs@gmail.com>
2021-08-04 23:42:04 -0400

    perf: optimize query performance

---
+++ user_details.py
@@ -0,0 +1,10 @@
+
+// Minor tweak fe9915fa-0b2f-427a-a728-9a3b040c4359
+
+// Minor tweak e5a2ecf0-9a29-4dc4-a293-bc7f45fe9d61
+
+// Minor tweak 29b1c81a-0ce5-4044-a0a6-c29be16994a2
+
+// Minor tweak 6bf0d71e-300e-430c-92d2-b94800c33004
+
+// Minor tweak a24c002c-75ec-4fc9-a45c-956b54d3093a
