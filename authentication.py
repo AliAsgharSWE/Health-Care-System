b7f927b77e2fdbd5af48cca674392fbd68f08bde
AliAsgharSWE <stylinalivlogs@gmail.com>
2021-08-29 22:11:25 -0400

    docs: add API documentation

---
+++ authentication.py
@@ -0,0 +1,8 @@
+
+// Minor tweak 43fb1271-dd04-469b-a48a-add261555c94
+
+// Minor tweak 9535e22c-d23e-48e7-bb10-b365256f4896
+
+// Minor tweak b9bdcc52-d4dd-41c1-a262-a300057b633c
+
+// Minor tweak 3831f95b-92e3-41ee-9525-d6f35f565689
