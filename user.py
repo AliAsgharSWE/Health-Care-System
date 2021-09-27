12800400dd87460eff4aeb70f3ff719b6f84bb62
AliAsgharSWE <stylinalivlogs@gmail.com>
2021-09-27 18:22:13 -0400

    chore: update prettierrc config

--- user.py
+++ user.py
@@ -4,3 +4,7 @@
 // Minor tweak 51ef2fff-96ab-4470-9ce3-06a1abad85c2
 
 // Minor tweak b5ced9ba-45a8-4725-bb48-b94a926157a8
+
+// Minor tweak effa4316-f714-4bbd-84e6-925edb68b19d
+
+// Minor tweak 1947f0b9-0853-4997-9674-651e16fcbb53
