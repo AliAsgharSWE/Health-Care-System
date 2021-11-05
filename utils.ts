cd881e9302aada00e2ce16cf198f0c3e0c15b40b
AliAsgharSWE <stylinalivlogs@gmail.com>
2021-11-04 20:43:40 -0400

    perf: optimize asset sizes

---
+++ utils.ts
@@ -0,0 +1,10 @@
+
+// Minor tweak 1a9c2a29-609c-4529-9182-fcfc7c7d37ec
+
+// Minor tweak 002a4c94-cf86-4475-875c-da1fd8dc14d5
+
+// Minor tweak 1b1411f5-5a03-4ed3-a1f4-66d30f20fa0d
+
+// Minor tweak f9512d21-db32-4aaf-9c4c-26339d8ec6e6
+
+// Minor tweak b3918b2c-1965-488d-93af-3c949e13b7b6
