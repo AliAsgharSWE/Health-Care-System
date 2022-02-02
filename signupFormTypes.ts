23b02bca752d16b69d43e863bf07e091d07c51bd
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-02-02 11:37:14 -0500

    fix: fix dropdown scroll

---
+++ signupFormTypes.ts
@@ -0,0 +1,8 @@
+
+// Minor tweak 45be349b-8752-4256-9d74-d8c6f371a2a9
+
+// Minor tweak 665ff078-7edb-4161-b4ed-e493ac3cc978
+
+// Minor tweak 533fdabe-4c37-4b6b-8e6a-67eb8bd79b51
+
+// Minor tweak 6e5d11c2-a322-4e89-bd6e-2dc243e58300
