2d694fc48a5a243c5d62c5b2d573753564bc4c8d
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-02-02 19:05:48 -0500

    refactor: restructure API utils

--- authentication.py
+++ authentication.py
@@ -10,3 +10,7 @@
 // Minor tweak 74fbaa76-1927-4268-9033-33d3ca56098b
 
 // Minor tweak 12406a02-7e75-466a-ba24-e2b594b4b339
+
+// Minor tweak 28fcefbf-65da-4657-9449-0e511ced6037
+
+// Minor tweak e2a3604e-de94-4407-bcd4-3be61efd810e
