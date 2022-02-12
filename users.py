cee2e157c3c80274b0b891fd828f46d71557003d
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-02-12 12:50:46 -0500

    feat: add confirmation dialog

--- users.py
+++ users.py
@@ -4,3 +4,7 @@
 // Minor tweak 081d0329-5400-4a55-adcd-e73aaee7c69b
 
 // Minor tweak e6eac36b-1ec8-4aef-ad2e-e9a288109f44
+
+// Minor tweak 9eada28a-cb90-477f-a960-e12f18de6660
+
+// Minor tweak 95fc4617-7634-4623-9c2e-680378dc1139
