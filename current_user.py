595ba28b2e08f894e4114d93ee9a240d43de7978
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-02-13 08:06:52 -0500

    chore: minor JS optimizations

--- current_user.py
+++ current_user.py
@@ -4,3 +4,7 @@
 // Minor tweak 50becf28-e612-42dc-bcb8-9f6ef8279194
 
 // Minor tweak c06f2ba9-ae2e-4a3e-b346-83b72a69e12b
+
+// Minor tweak bfed96ed-ce3f-4593-a1ab-9eee173438bd
+
+// Minor tweak 0c3629ab-a2e3-4921-b522-1ce28d7270f7
