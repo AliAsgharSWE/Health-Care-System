a1ae8d60fdc08e34952aa9cc1ad373b5ef0e845c
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-03-29 20:26:58 -0400

    refactor: update dashboard components

---
+++ appointmentSchema.ts
@@ -0,0 +1,8 @@
+
+// Minor tweak a94874c5-be96-46fc-8067-454ccf5cddd9
+
+// Minor tweak 54cc79a2-f90c-49e4-9dc6-057ae60fe274
+
+// Minor tweak fa8ff4a5-a944-4b00-bb14-3e5ad160dd86
+
+// Minor tweak 44767676-faa6-43db-90ad-72dc85f4a8ee
