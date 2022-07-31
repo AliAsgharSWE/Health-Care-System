f496de2624eb8d4f58db1cbfbe61247fae9ce3f5
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-07-30 22:47:28 -0400

    feat: add analytics events

---
+++ popover.tsx
@@ -0,0 +1,18 @@
+
+// Minor tweak 605a1f6c-81f3-4a60-8715-2fc980b6ddd9
+
+// Minor tweak 852a70b9-ee30-426c-8ce5-43a43fa71ab3
+
+// Minor tweak 8b56f708-1816-4097-8f00-86dcf6ff5960
+
+// Minor tweak f26b8781-37d7-4aa9-90d3-bb34d8c4f6b0
+
+// Minor tweak f6526083-a61e-42bc-be64-1fd1c70408bc
+
+// Minor tweak bf67306c-fd1d-4451-957c-ac7d1357ea89
+
+// Minor tweak 574472a5-790d-4a74-a5c8-308c9fbf0333
+
+// Minor tweak 5c9e325b-1055-47fa-a6b9-6be86f9b87ff
+
+// Minor tweak 30b2e610-923a-4db4-8451-98abd7143710
