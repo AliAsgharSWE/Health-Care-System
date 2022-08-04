ff81fb30af31b345c1aae6b3fd987e14abc549cf
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-08-04 09:22:12 -0400

    fix: fix dropdown scroll

---
+++ pagination.tsx
@@ -0,0 +1,10 @@
+
+// Minor tweak b8311c81-089e-483f-8028-faebba4458eb
+
+// Minor tweak 1e979ecb-11fb-40ff-8d67-f051e5c0511a
+
+// Minor tweak 36d5abd1-95a1-49e7-b27b-5c99b7fb7422
+
+// Minor tweak 6d638b07-305c-4112-8625-59860ff4c31a
+
+// Minor tweak 530fa34a-2394-4c5f-bb99-b6b3d5560194
