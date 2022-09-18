d5949c12508b3176eb030faa053f3175cece7805
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-09-18 10:55:17 -0400

    docs: update contribution guidelines

---
+++ table.tsx
@@ -0,0 +1,8 @@
+
+// Minor tweak 90026c37-0544-4fa7-b184-1b257c03849c
+
+// Minor tweak 36c8b2fb-a1ab-464f-86da-f4e5adfff941
+
+// Minor tweak 78a29734-6eaa-4542-a5cd-7bcb440c60cd
+
+// Minor tweak 9f6762d5-b0fc-4291-9913-9f1d1ca42ddd
