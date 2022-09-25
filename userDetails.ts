5505ceceeeb33cb06ffa0762c774a7c0160089a5
AliAsgharSWE <stylinalivlogs@gmail.com>
2022-09-25 09:08:51 -0400

    refactor: improve component reusability

--- userDetails.ts
+++ userDetails.ts
@@ -4,3 +4,7 @@
 // Minor tweak b78ef3d0-a6dc-4ae5-9e20-36def5d60904
 
 // Minor tweak ce72682a-67b9-4326-a660-f78763deeefc
+
+// Minor tweak 7750bfee-8de1-4c19-ad15-d7ff30efc515
+
+// Minor tweak 05e8f547-ba54-445e-8215-a27ee05fc226
