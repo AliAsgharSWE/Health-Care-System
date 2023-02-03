7c76e9e769faf9baaad3ec7a2478311107bc2fa6
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-02-03 13:23:54 -0500

    fix: correct flexbox layout

--- userRegistration.ts
+++ userRegistration.ts
@@ -6,3 +6,9 @@
 // Minor tweak e3fa3fd0-f795-4708-8318-5344034ea178
 
 // Minor tweak 7b4cfa77-4761-4fc6-b9bc-f71599c5bde1
+
+// Minor tweak ad27b045-a8b5-46d1-9fab-8461995be70a
+
+// Minor tweak 2754dc58-8869-4a04-bcdb-13c13cd98c24
+
+// Minor tweak 84009b8d-75fe-48e3-9158-8a99b0183824
