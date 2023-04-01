cbd27863d8a16c5ec5014596de1cc54ce3dd7885
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-04-01 12:48:55 -0400

    perf: reduce bundle size

--- config.py
+++ config.py
@@ -2,3 +2,9 @@
 // Minor tweak 34d06536-7ab2-4bf8-bcb6-f1d8bd177ec7
 
 // Minor tweak 49114fae-e1e7-41b9-bcea-91cc5416efa1
+
+// Minor tweak 7341d482-05b9-4b24-8f1c-b37147fd0cf3
+
+// Minor tweak 4d0dc738-35ad-45e2-a95b-715d02a5d06d
+
+// Minor tweak 17b177e0-f0a7-4e5f-a4b1-63c7dd0f6fbd
