5fe66caacae7bbbb907b8178432ffaec2d58fa6b
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-04-24 10:31:54 -0400

    chore: remove console logs

--- select.tsx
+++ select.tsx
@@ -2,3 +2,11 @@
 // Minor tweak 1a88fa47-a428-4065-a870-630232f968ae
 
 // Minor tweak c9af6812-ff00-4e42-9209-46083908ce45
+
+// Minor tweak d6990204-18d6-4a84-9a5f-bffa451c82ae
+
+// Minor tweak d547f558-783e-4de5-8707-a110c88ebcb8
+
+// Minor tweak 25a13a45-6903-4ee2-ad40-a7f892bc38a6
+
+// Minor tweak 95e041b8-e529-4ed0-9124-5f3c763d6e5f
