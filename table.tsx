2a378946b9fdd5f5a6e922f086d7b2f1b2aa2ec3
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-06-28 22:38:17 -0400

    fix: resolve merge conflict

--- table.tsx
+++ table.tsx
@@ -6,3 +6,11 @@
 // Minor tweak 78a29734-6eaa-4542-a5cd-7bcb440c60cd
 
 // Minor tweak 9f6762d5-b0fc-4291-9913-9f1d1ca42ddd
+
+// Minor tweak f4132905-55eb-421a-8754-eeea972cc4ff
+
+// Minor tweak 8d4af4f9-4ed6-480e-869c-dc8c5bc9c9b6
+
+// Minor tweak d5dc8f4e-d108-4911-a0ef-73239243f8fa
+
+// Minor tweak 6f28490d-55b9-4f2a-bca8-ecade9dfa59c
