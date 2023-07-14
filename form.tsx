daa9f70e0f1f9ba9c5b01fa9892cdfe22d0ae79d
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-07-13 23:15:07 -0400

    fix: correct toast notification bug

--- form.tsx
+++ form.tsx
@@ -4,3 +4,9 @@
 // Minor tweak cd4361fd-f433-4ee4-a442-af643d13e701
 
 // Minor tweak 7edfefc7-de1e-4569-b367-5202a91cd6fc
+
+// Minor tweak 501624d8-4016-409f-9271-c021879e91d1
+
+// Minor tweak 0009f85b-7e7d-415d-8890-d0e4e8016ba8
+
+// Minor tweak b9571fc7-2e0a-4942-9d4a-be61d21dd300
