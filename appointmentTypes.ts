19954c22654cc469eb99444be257534504311db2
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-08-21 09:08:49 -0400

    perf: optimize image compression

--- appointmentTypes.ts
+++ appointmentTypes.ts
@@ -10,3 +10,7 @@
 // Minor tweak 97955c0c-5fb7-405a-b10a-565f0c06e206
 
 // Minor tweak 493f3fbd-1c12-4fc5-b037-cc983fd3d19b
+
+// Minor tweak b8817e42-f41b-4389-9613-e699c92cbd00
+
+// Minor tweak 4f2bd014-662b-4d63-a128-41c681fb1200
