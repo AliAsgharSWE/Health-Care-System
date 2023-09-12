9989e38c00d4adf69934df96282cf30da1e35f4d
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-09-12 08:14:43 -0400

    feat: add error tracking

--- session.py
+++ session.py
@@ -8,3 +8,7 @@
 // Minor tweak 6fa05428-ad8e-4db4-b6e8-fdb1765cce4f
 
 // Minor tweak a8aa2eeb-0e13-4aa8-bfc0-78eab5a5e7a0
+
+// Minor tweak 67825d4b-620c-45b6-a748-08fde92bdb57
+
+// Minor tweak 881d2920-f1cf-46d6-8605-4808d5e9546a
