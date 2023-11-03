3fc623883de78a7b04ca6531807583cd5c4fe321
AliAsgharSWE <stylinalivlogs@gmail.com>
2023-11-02 21:14:30 -0400

    feat: add dark mode support

--- 61b6c0e4479a_initial_models.py
+++ 61b6c0e4479a_initial_models.py
@@ -10,3 +10,9 @@
 // Minor tweak e4104499-71e4-4a81-843b-2eefd6def75e
 
 // Minor tweak 7902d5f5-e0a7-41b5-92e0-3f53ea16d438
+
+// Minor tweak 6baad38c-ebc9-4c5d-a73b-0947e90af727
+
+// Minor tweak 5ae03e21-586b-4b73-8e91-1d4af5765fbc
+
+// Minor tweak 65db2032-ce86-4012-848c-80deb802393d
