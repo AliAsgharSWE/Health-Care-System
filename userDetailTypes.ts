e4353cc96ae5652da4cbe45a5f479e496e701600
AliAsgharSWE <stylinalivlogs@gmail.com>
2024-04-13 18:42:04 -0400

    perf: optimize query performance

--- userDetailTypes.ts
+++ userDetailTypes.ts
@@ -8,3 +8,11 @@
 // Minor tweak 4af93332-de32-4e21-b8c8-e710f798fa02
 
 // Minor tweak 741f24db-d2e1-4ac6-a012-0546e8617f35
+
+// Minor tweak 4b3fb62e-2318-4ceb-8bac-20107b9fe2d7
+
+// Minor tweak 28a64aa3-5740-4f70-b80d-0bfab8f50c7d
+
+// Minor tweak 97c61841-e50a-48fc-9113-30f2375f9f1a
+
+// Minor tweak 713c13d1-9082-4eda-bf6a-746f7768a1f8
