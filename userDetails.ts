32719d9f0d5bf062791b1209bf450a7ecca37710
AliAsgharSWE <stylinalivlogs@gmail.com>
2024-05-16 22:41:42 -0400

    refactor: refactor notification system

--- userDetails.ts
+++ userDetails.ts
@@ -10,3 +10,13 @@
 // Minor tweak 05e8f547-ba54-445e-8215-a27ee05fc226
 
 // Minor tweak 5cfe094c-e975-4f30-8c8c-1b3e440d6cba
+
+// Minor tweak c236d554-94a1-44ac-bc2b-41a41e026cd9
+
+// Minor tweak 74f6ab1b-1713-4a01-a9c0-55c1efda0866
+
+// Minor tweak 61b6e46b-e3f9-489b-ba4a-f12dbc86de82
+
+// Minor tweak 886f0352-43c3-463d-86c5-555b836e48be
+
+// Minor tweak 1c714b1c-da6a-43aa-893a-1330215a7cb3
